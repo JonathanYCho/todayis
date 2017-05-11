c2152efb950f9b7a45043d43b5a630657ffc9425
Istvan <ifulop@gmail.com>
2017-05-11 13:52:34 -0700

    manager

--- TodayIs/ViewController.swift
+++ TodayIs/ViewController.swift
@@ -11,22 +11,27 @@ import CoreLocation
 
 
 class ViewController: UIViewController {
-
+    var locationManager: CLLocationManager?
+    var startLocation: CLLocation?
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var celebrationLabel: UILabel!
-    
     @IBOutlet weak var map: MKMapView!
 
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let centerLocation = CLLocationCoordinate2DMake(-27, 153)
-        let mapSpan = MKCoordinateSpanMake(0.01, 0.01)
-        let mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
-        self.map.setRegion(mapRegion, animated: true)
-        map.userTrackingMode = .follow
+//        let centerLocation = CLLocationCoordinate2DMake(-27, 153)
+//        let mapSpan = MKCoordinateSpanMake(0.01, 0.01)
+//        let mapRegion = MKCoordinateRegionMake(centerLocation, mapSpan)
+//        self.map.setRegion(mapRegion, animated: true)
+//        map.userTrackingMode = .follow
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager?.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,10 +39,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
-
 }
 //I hope I don't accidentally merge the branches anytime soon.
 //Red two standing by.
 //Stay on target!
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else {
+            guard let latest = locations.first else { return }
+            let distanceInMeters = startLocation?.distance(from: latest)
+            print("distance in meters: \(String(describing: distanceInMeters!))")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager?.startUpdatingLocation()
+            locationManager?.allowsBackgroundLocationUpdates = true
+        }
+    }
+    
+}
