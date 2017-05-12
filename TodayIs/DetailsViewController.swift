09ba9e7405c586e984526ef1a728e7f34fe4c138
Jonathan Cho <jonathanycho@gmail.com>
2017-05-12 11:56:25 -0700

    2nd view controller and page plus border

---
+++ TodayIs/DetailsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailsViewController.swift
+//  TodayIs
+//
+//  Created by Jonathan Cho on 5/12/17.
+//  Copyright © 2017 Coding Dojo. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.layer.borderWidth = 7.5
+        self.view.layer.borderColor = UIColor.yellow.cgColor
+        
+        print (self.view)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
